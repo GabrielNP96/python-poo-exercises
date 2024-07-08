35af1da47d269e5cfb1ed6a56bfe8d32cf9fd04a
GabrielNP96 <gabrielnp1996@gmail.com>
2024-07-08 07:06:41 -0300

    classe criada

--- 01-conta-bancaria/models/bank -account.py
+++ 01-conta-bancaria/models/bank -account.py
@@ -1,4 +1,17 @@
 class Bank_acount:
     def __init__(self, holder_name, balance):
         self.holder_name = holder_name
-        self.balance = balance
+        self.balance = float(balance)
+
+    def cash_deposit(self,num):
+        self.balance = self.balance + float(num)
+
+        return print(f'O saldo é de R${self.balance} agora')
+    
+    def withdraw_money(self,number):
+        self.balance = self.balance - float(number)
+
+        return print(f'Você sacou R${number} e restou na sua conta ${self.balance}')
+    
+    def check_balance(self):
+        return print(f'você tem R${self.balance}')

ed4fb1acb2bacd925594c59010285a9a48d41994
GabrielNP96 <gabrielnp1996@gmail.com>
2024-07-09 06:34:24 -0300

    funcional

--- 01_conta_bancaria/CLI_interface.py
+++ 01_conta_bancaria/CLI_interface.py
@@ -1,16 +1,20 @@
-from models.bank_account import Bank_acount
+from Bank_account import Bank_acount
+
+def create_bank_account():
+    i = True
+    while i == True:
+        try:
+            user_name = input('Digite seu nome: ')
+            user_balance = float(input('Saldo inicial da sua conta bancária: '))
+
+            return Bank_acount(user_name, user_balance)
+        except ValueError:
+             print('Erro...\nValores inválidos...')
+        
 
 
 def cli_interface():
-    i = True
-    while i == True:
-        user_name = input('Digite seu nome: ')
-        user_balance = float(input('Saldo inicial da sua conta bancária: '))
-        if len(user_name) > 0 and user_balance == float:
-            user_bank_account = Bank_acount(user_name, user_balance)
-            break
-        else:
-            print('Digite um nome de usuário e um saldo incial válido.')
+    user_bank_account = create_bank_account()
     i = True
     while i == True:
        option =  int(input('Opções:\n Digite 1 para depositar dinheiro\nDigite 2 para sacar\nDigite 3 para ver o saldo\nDigite 4 para fechar o programa\nDigite aqui: '))
@@ -18,19 +22,17 @@ def cli_interface():
        match option:
            case 1:
                cash = float(input('Digite o valor que deseja depositar: '))
-               Bank_acount.cash_deposit(cash)
+               user_bank_account.cash_deposit(cash)
                print(f'Deposito de R${cash} realizado.\n')
                continue
            case 2:
                cash = float(input('Digite o valor que deseja sacar: '))
-               Bank_acount.withdraw_money(cash)
+               user_bank_account.withdraw_money(cash)
                print(f'Saque de R${cash} realizado.\n')
                continue
            case 3:
-               Bank_acount.check_balance()
+               user_bank_account.check_balance()
                continue
            case 4:
                print('programa encerrado...\n')
                break
-
-cli_interface()
---
+++ 01_conta_bancaria/app.py
@@ -0,0 +1,6 @@
+from CLI_interface import cli_interface
+
+def __main__():
+    cli_interface()
+if __name__ == '__main__':
+    __main__()

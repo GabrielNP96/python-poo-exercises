7c44bb98f574533667025ea5299931b7f5a29915
GabrielNP96 <gabrielnp1996@gmail.com>
2024-07-10 19:59:30 -0300

    metodos prontos

--- 01_conta_bancaria/model/Bank_account.py
+++ 01_conta_bancaria/model/Bank_account.py
@@ -5,3 +5,22 @@ class Bank_account(Agency):
         super().__init__(name, address, number)
         self.__holder = holder
         self.__balance = balalce
+
+
+    def __str__(self):
+        f'Nome do banco: {self._name}\nEndereço do Banco: {self._address}\nNúmero da agência: {self._number}\nTitular da conta: {self.__holder}\nSaldo: {self.__balance}R$'
+
+    def deposit(self,number):
+        if float(number):
+           self._balance += float(number)
+           return print(f'Você depositou {number}R$')
+        return print('Erro... \nVocê deve digitar um número.')
+    
+    def to_withdraw(self, number):
+        if float(number):
+            self.__balance -= float(number)
+            return print(f'Você sacou {number}R$')
+        return print('Erro... \nVocê deve digitar um número.')
+    
+    def show_balance(self):
+        return print(f'Seu saldo atual é {self.__balance}R$')
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Este repositório contém exercícios de Programação Orientada a Objetos (POO)
 
 ### 01 - Conta Bancária
 
-Crie uma classe ContaBancaria que modele uma conta bancária simples. A classe deve ter atributos para o nome do titular, endereço,agencia e número, saldo da conta Implemente métodos para:
+Crie uma classe ContaBancaria que modele uma conta bancária simples. A classe deve ter atributos para o nome do titular, endereço,agência e número, saldo da conta Implemente métodos para:
 
 - **Depositar**: Receber um valor como parâmetro e atualizar o saldo da conta
 - **Sacar**: Receber um valor como parâmetro, verificar se há saldo suficiente e limite disponível, e atualizar o saldo caso seja possível.
